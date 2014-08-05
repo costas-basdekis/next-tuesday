be41ee4608e38423c9938eb4dc840763693c66b5
Costas Basdekis <costas.basdekis@gmail.com>
2014-08-05 20:58:26 +0000

    Move templates and typo

--- next_tuesday/views.py
+++ next_tuesday/views.py
@@ -3,8 +3,8 @@ from django.views.generic import TemplateView
 class NextTuesday(TemplateView):
 	template_name = "nexttuesday.html"
 
-	def get_context(self):
-		context = super(NextTuesday, self).get_context()
+	def get_context_data(self, **kwargs):
+		context = super(NextTuesday, self).get_context_data(**kwargs)
 
 		context['phrase'] = "I can't thing of anything"
 
