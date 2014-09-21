a097a16d9a74be89382c6b8f1a9b3c5557b934c9
Costas Basdekis <costas.basdekis@gmail.com>
2014-09-21 14:05:07 +0000

    Redirect all other urls to index

--- next_tuesday/urls.py
+++ next_tuesday/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.views import generic
+
 from . import views
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', views.NextTuesdayView.as_view(), name='home'),
-    url(r'^(?P<word>\w+)/$', views.NextTuesdayView.as_view(), name='home-word'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^(?P<word>\w+)/?$', views.NextTuesdayView.as_view(), name='home-word'),
+    url(r'^.*$', generic.RedirectView.as_view(url='/'), name='home-rest'),
 
     #url(r'^admin/', include(admin.site.urls)),
 )
